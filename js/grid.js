b08e6d57ab974c4b9d56eac8b1c49820b5ee60e5
Matheus <pegdevcolab@gmail.com>
2024-08-08 13:58:45 -0300

    :sparkles: feat: Request da tabela de anúncios feita

--- js/grid.js
+++ js/grid.js
@@ -1,40 +1,52 @@
-const columnDefs = [
-    { headerName: "TÍTULO DO ANÚNCIO", field: "imageName", cellRenderer: imageRenderer, autoHeight: true },
-    { headerName: "STATUS", field: "status" }
-];
+document.addEventListener('DOMContentLoaded', function() {
+    const columnDefs = [
+        { headerName: "Imagem", field: "thumbnail", cellRenderer: imageRenderer },
+        { headerName: "Título", field: "title" },
+        { headerName: "Preço", field: "price" },
+        { headerName: "Quantidade Vendida", field: "sold_quantity" },
+        { headerName: "Quantidade Disponível", field: "available_quantity" },
+        { headerName: "Tipo de Envio", field: "logistic_type" },
+        { headerName: "Tipo de Anúncio", field: "listing_type_id" }
+    ];
 
-const rowData = [
-    { imageName: "list001", productName: "Produto A", productId: "ID001", status: true },
-    { imageName: "list002", productName: "Produto B", productId: "ID002", status: true },
-    { imageName: "list003", productName: "Produto C", productId: "ID003", status: false }
-];
-
-function imageRenderer(params) {
-    const imagePath = `images/list/${params.value}.png`;
-
-    const productName = params.data.productName;
-    const productId = params.data.productId;
-
-    return `
-        <div style="display: flex; align-items: center;">
-            <img src="${imagePath}" alt="${params.value}" style="width: 100px; height: auto; margin-right: 10px;">
-            <div>
-                <div><strong></strong>${productName}</div>
-                <div><strong></strong>${productId}</div>
+    function imageRenderer(params) {
+        return `
+            <div style="display: flex; align-items: center;">
+                <img src="${params.value}" alt="thumbnail" style="width: 100px; height: auto;margin-right: 10px;">
             </div>
-        </div>
-    `;
-}
-
-const gridOptions = {
-    columnDefs: columnDefs,
-    rowData: rowData,
-    defaultColDef: {
-        resizable: true
+        `;
     }
-};
 
-document.addEventListener('DOMContentLoaded', function () {
-    const eGridDiv = document.querySelector('#myGrid');
-    new agGrid.Grid(eGridDiv, gridOptions);
+    let getData = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:5000/searchTeste');
+    
+            console.log('Response:', response);
+    
+            if (!response.ok) {
+                throw new Error(`Erro de conexão: ${response.statusText}`);
+            }
+    
+            const data = await response.json();
+            console.log('Data:', data);
+    
+            const gridOptions = {
+                columnDefs: columnDefs,
+                rowData: data,
+                defaultColDef: {
+                    resizable: true
+                }
+            };
+    
+            new agGrid.Grid(document.querySelector('#myGrid'), gridOptions);
+    
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    };
+    
+    getData();
+    
+    
+    
 });
