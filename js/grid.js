b2a7db5ea5ab2c98d7694e4fa8246968f40fa2ec
Matheus <pegdevcolab@gmail.com>
2024-08-08 14:27:02 -0300

    :sparkles: feat: Criação do filtro de pesquisa pelo título

--- js/grid.js
+++ js/grid.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    function replaceAccents(value) {
+        return value
+            .replace(new RegExp("[àáâãäå]", "g"), "a")
+            .replace(new RegExp("æ", "g"), "ae")
+            .replace(new RegExp("ç", "g"), "c")
+            .replace(new RegExp("[èéêë]", "g"), "e")
+            .replace(new RegExp("[ìíîï]", "g"), "i")
+            .replace(new RegExp("ñ", "g"), "n")
+            .replace(new RegExp("[òóôõøö]", "g"), "o")
+            .replace(new RegExp("œ", "g"), "oe")
+            .replace(new RegExp("[ùúûü]", "g"), "u")
+            .replace(new RegExp("[ýÿ]", "g"), "y")
+            .replace(new RegExp("\\W", "g"), "")
+            .toLowerCase();
+    }
+
+    const filterParams = {
+        textFormatter: replaceAccents,
+    };
+    
     const columnDefs = [
-        { headerName: "Imagem", field: "thumbnail", cellRenderer: imageRenderer },
-        { headerName: "Título", field: "title" },
+        { headerName: "Imagem", field: "thumbnail", cellRenderer: imageRenderer, sortable: false },
+        { headerName: "Título", field: "title", filter: 'agSetColumnFilter', filterParams: filterParams },
         { headerName: "Preço", field: "price" },
         { headerName: "Quantidade Vendida", field: "sold_quantity" },
         { headerName: "Quantidade Disponível", field: "available_quantity" },
@@ -29,10 +50,11 @@ document.addEventListener('DOMContentLoaded', function() {
     
             const data = await response.json();
             console.log('Data:', data);
-    
+
             const gridOptions = {
                 columnDefs: columnDefs,
                 rowData: data,
+                rowHeight: 100,
                 defaultColDef: {
                     resizable: true
                 }
@@ -46,7 +68,4 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     
     getData();
-    
-    
-    
 });
