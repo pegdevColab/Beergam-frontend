90343cb9e143c46c4bb60b98187bd0421b00a632
Matheus <pegdevcolab@gmail.com>
2024-08-09 15:58:21 -0300

    feat: Criação dos anúncios mais vendidos e sua participação total nas vendas

--- js/progressao-circular.js
+++ js/progressao-circular.js
@@ -23,3 +23,29 @@ $('.circle').each(function() {
         }
     });
 });
+
+$('.circle2').each(function() {
+    var $this = $(this);
+    var percentageText = $this.find('span').text();
+    var value = parseFloat(percentageText.replace('%', '')) / 100;
+
+    if (value <= 0.30) {
+        fillColor = ["red", "darkred"];
+    } else if (value <= 0.70) {
+        fillColor = ["yellow", "orange"];
+    } else {
+        fillColor = ["green", "lightgreen"];
+    }
+
+    if (isNaN(value) || value < 0 || value > 1) {
+        value = 0;
+    }
+
+    $this.circleProgress({
+        value: value,
+        size: 80,
+        fill: {
+            gradient: fillColor
+        }
+    });
+})
